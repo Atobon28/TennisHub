c90dc140d983cde52a2fbf8715cfbeae698aca3b
Ana <anita.tobon28@gmail.com>
2026-05-19 15:46:54 -0500

    chore: use centralized context exports

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
-import { CourtsProvider } from "./context/CourtsContext";
-import { TournamentsProvider } from "./context/TournamentsContext";
-import { MatchesProvider } from "./context/MatchesContext";
-import { CoachesProvider } from "./context/CoachesContext";
-import { PlayersProvider } from "./context/PlayersContext";
-import { ProfileProvider } from "./context/ProfileContext";
-import { DashboardProvider } from "./context/DashboardContext";
+import {
+  AuthProvider,
+  CourtsProvider,
+  TournamentsProvider,
+  MatchesProvider,
+  CoachesProvider,
+  PlayersProvider,
+  ProfileProvider,
+  DashboardProvider,
+} from "./context";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
