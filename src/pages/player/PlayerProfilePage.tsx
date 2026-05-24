00d6fe2e93785cf39ba6dbbff64197420ceaa394
Juancastrog10 <juancastrogrimaldi10@gmail.com>
2026-05-24 13:52:32 -0500

    Fin Toast

--- src/pages/player/PlayerProfilePage.tsx
+++ src/pages/player/PlayerProfilePage.tsx
@@ -9,6 +9,7 @@ import type { Match } from "../../context/MatchesContext";
 import type { Tournament } from "../../context/TournamentsContext";
 import "../../styles/player-profile.css";
 import player1 from "../../assets/player-1.jpg";
+import { useToast } from "../../context/ToastContext";
 
 const categoryOptions = [
   "First Category",
@@ -65,20 +66,14 @@ const getCategoryLevel = (category: string) => {
 function PlayerProfilePage() {
   const navigate = useNavigate();
   const { userData, refreshUserData } = useAuth();
+  const { showToast } = useToast();
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const {
-    playerMatches,
-    loadPlayerMatches,
-    leaveExistingMatch,
-    removeMatch,
-  } = useMatches();
+  const { playerMatches, loadPlayerMatches, leaveExistingMatch, removeMatch } =
+    useMatches();
 
-  const {
-    playerTournaments,
-    loadPlayerTournaments,
-    unregisterFromTournament,
-  } = useTournaments();
+  const { playerTournaments, loadPlayerTournaments, unregisterFromTournament } =
+    useTournaments();
 
   const {
     uploadAvatar,
@@ -160,13 +155,16 @@ function PlayerProfilePage() {
       await refreshUserData();
 
       setAvatarMsg("Avatar updated successfully.");
+      showToast("Avatar uploaded successfully.", "success");
     } catch (error) {
       console.error("Error uploading avatar:", error);
 
       if (error instanceof Error) {
         setAvatarMsg(error.message);
+        showToast("Error uploading image.", "error");
       } else {
         setAvatarMsg("Error uploading avatar. Please try again.");
+        showToast("Error uploading image.", "error");
       }
     } finally {
       setUploadingAvatar(false);
@@ -229,7 +227,7 @@ function PlayerProfilePage() {
       });
 
       await refreshUserData();
-
+      showToast("Profile saved successfully.", "success");
       setPlayerCategory(tempCategory);
       setTempCategory(tempCategory);
       setShowCategoryModal(false);
